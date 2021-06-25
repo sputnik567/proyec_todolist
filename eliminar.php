1caad14464d136d98d56d307cc58faf22cdc86af
sputnik567 <84925450+sputnik567@users.noreply.github.com>
2021-06-25 13:21:52 -0500

    Create eliminar.php

---
+++ eliminar.php
@@ -0,0 +1,17 @@
+<?php  
+	if (!isset($_GET['id'])) {
+		exit();
+	}
+
+	$codigo = $_GET['id'];
+	include 'model/conexion.php';
+	$sentencia = $bd->prepare("DELETE FROM alumno WHERE id_alumno = ?;");
+	$resultado = $sentencia->execute([$codigo]);
+
+	if ($resultado === TRUE) {
+		header('Location: index.php');
+	}else{
+		echo "Error";
+	}
+
+?>
