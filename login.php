e8496f40f88d145009be9624bb24fe8a38d21c0f
sputnik567 <84925450+sputnik567@users.noreply.github.com>
2021-06-25 13:11:39 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,27 +1,39 @@
-<?php 
-	session_start();
-	if (isset($_SESSION['nombre'])) {
-		header('Location: index.php');
+<? php 
+	session_start ();
+	if ( isset ( $ _SESSION [ 'nombre' ])) {
+		encabezado ( 'Ubicación: index.php' );
 	}
 ?>
 
+
 <!DOCTYPE html>
 <html>
+<center>
+
 <head>
-	<title>Iniciar sesion</title>
-	<meta charset="utf-8">
+	<title><strong>Iniciar sesion</strong></title>
+	<meta charset="utf-9">
 </head>
 <body>
+	<body style=" margin: 0;padding: 0;width: 70%;height: 200px;position: relative ; background: #2E9AFE;">
+
+	<div class="login">
+
 	<center>
-		<form method="POST" action="loginProceso.php">
-			<label>Usuario: </label>
+
+		<center style="border: 1px solid #E1E1E1; border-radius: 5px;padding: 17px;width: 300px;position: absolute;top: 100%; left: 50%; transform: translate(20%,20%); background: #E0E0F8;">
+			
+		<form method="POST" action="loginProceso.php ">
+			
+			<label><em><strong>Usuario</strong></em></label>
 			<input type="text" name="txtUsu">
 			<br>
-			<label>Password: </label>
+			<label><em><strong>Password</strong></em></label>
 			<input type="password" name="txtPass">
 			<br>
 			<input type="submit" value="Iniciar sesión">
 		</form>
 	</center>
 </body>
+</center>
 </html>
